cf930b8ac38424ecb44e7c9a5073b14f99f0e600
Benji <benji@jibber.wtf>
2022-05-20 14:32:31 -0700

    updating meta tag to support app clips

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta
-          name='apple-itunes-app'
-          content='app-id=863YH82QFN, app-clip-bundle-id=com.Jibber-Inc.iOS.Clip, affiliate-data=myAffiliateData, app-argument=myAppArgument'
-        />
+      <meta name="apple-itunes-app" content="app-id=863YH82QFN, app-clip-bundle-id=com.Jibber-Inc.iOS.Clip, app-clip-display=card"/>
       </Head>
       <h1>Onboarding</h1>
     </>
