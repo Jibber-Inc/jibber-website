a5780b798374b2acb40fb1b9c2862f1216232926
Benji <benji@jibber.wtf>
2022-05-20 14:58:02 -0700

    adding the og image to support app clips

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -9,7 +9,7 @@ export const Header = () => {
         name='description'
         content='Jibber is a messaging app for those you are closest too. With a reimagined experience designed to encourage empathy and understanding.'
       />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/white_logo_round.png' />
     </Head>
   );
 };
